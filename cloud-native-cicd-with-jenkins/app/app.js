69765c0094a03ccd03ae5ccec2408205f5287936
Bhavin21187 <64798749+Bhavin21187@users.noreply.github.com>
2025-08-12 17:21:08 +0530

    Add files via upload

---
+++ cloud-native-cicd-with-jenkins/app/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Cloud Native CI/CD Pipeline with Jenkins and Kubernetes!');
+});
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
